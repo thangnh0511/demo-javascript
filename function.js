f3c0381d577eea8ff62e5cd515d867ee2c7b0d35
kubokid <thangnh0511@gmail.com>
2021-08-16 22:29:08 +0700

    update

--- function.js
+++ function.js
@@ -27,3 +27,31 @@ function marilynMessage() {
     <br>
     <p>${address}, ngày ${date} tháng ${month} năm ${year}."</p>`;
 }
+
+function currencyExchange() {
+    let money = document.getElementById('money').value;
+    var exFrom = document.getElementById('exchange-from').value;
+    var exTo = document.getElementById('exchange-to').value;
+
+    const exRate = document.getElementById('ex-rate').value;
+
+    if(exFrom === "usd") {
+        var exFrom = 1; 
+    } else {
+        var exFrom = exRate; 
+    }
+
+    if (exTo === "usd") {
+        var exTo = 1;
+    } else {
+        var exTo = exRate; 
+        
+    }
+    console.log(exFrom);
+    console.log(exTo);
+    let exMoney = +(money*exTo)/exFrom;
+    console.log(exMoney);
+    document.getElementById('exchange-result').innerHTML = (money*exTo)/exFrom;
+
+
+}

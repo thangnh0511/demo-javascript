f52d1ac0712a15f9d21a3f3f96137369a9c029e8
kubokid <thangnh0511@gmail.com>
2021-09-29 16:44:49 +0700

    update 11

--- 11-function-exe/function-exe-custom.js
+++ 11-function-exe/function-exe-custom.js
@@ -3,14 +3,30 @@ let products = [
     ['Iphone 13', 35000000, 'Apple'],
     ['Iphone 6s', 3000000, 'Apple'],
     ['Mi Mix', 12000000, 'Xiaomi'],
+    ['Mi band 6', 600000, 'Xiaomi'],
+    ['Iphone 13', 35000000, 'Apple'],
+    ['Iphone 6s', 3000000, 'Apple'],
+    ['Mi Mix', 12000000, 'Xiaomi'],
+    ['Mi band 6', 600000, 'Xiaomi'],
+    ['Iphone 13', 35000000, 'Apple'],
+    ['Iphone 6s', 3000000, 'Apple'],
+    ['Mi Mix', 12000000, 'Xiaomi'],
+    ['Mi band 6', 600000, 'Xiaomi'],
+    ['Iphone 13', 35000000, 'Apple'],
+    ['Iphone 6s', 3000000, 'Apple'],
+    ['Mi Mix', 12000000, 'Xiaomi'],
+    ['Mi band 6', 600000, 'Xiaomi'],
+    ['Iphone 13', 35000000, 'Apple'],
+    ['Iphone 6s', 3000000, 'Apple'],
+    ['Mi Mix', 12000000, 'Xiaomi'],
     ['Mi band 6', 600000, 'Xiaomi']
 ]
 
 function showListProduct() {
     let html = '';
     for(let i = 0; i<products.length; i++){
-        html += `<div class="catalogue-list">
-            <div class="catalogue-item">${i+1}</div>`;
+        html += `<div class="catalogue-list" onclick="getInfoConsole()">
+            <div id="id">${i+1}</div>`;
         for(let j=0; j<products[0].length; j++) {
             if(!(j==2)){
                 html += `<div class="text-align-left catalogue-item">${products[i][j]}</div>`;
@@ -31,3 +47,7 @@ function showListProduct() {
 
 showListProduct();
 
+
+function getInfoConsole(){
+    index = document.getElementById('')
+}
